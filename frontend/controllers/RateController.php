72d1f3a0eb39c409f78bfbeb41aefadca12eaff0
201502lisihao <735407073@qq.com>
2019-02-14 18:42:52 +0800

    完成评级页

--- frontend/controllers/RateController.php
+++ frontend/controllers/RateController.php
@@ -14,7 +14,8 @@ class RateController extends BaseController
 	 *
 	 */
 	public function actionIndex()
-	{
+	{	
+		//先读缓存，未命中再爬虫
 		//调用go接口去获取评级数据
 		$url = '127.0.0.1:9090';
 		$ch = curl_init();
@@ -23,20 +24,21 @@ class RateController extends BaseController
 		$response = curl_exec($ch);
 		$info = curl_getinfo($ch);
 		if (curl_errno($ch)) {
-			$response = $this->getRateByPHP();
+			//如果go没有抓成功，换php
+			$response = $this->_getRateByPHP();
 		}
 
 		curl_close($ch);
 		//var_dump($response);exit;
 
 		//处理抓回来的数据
-		//去掉里面不可显示的数据
-		$res = str_replace('(MISSING)','',$response);
-
-		return $this->render('index',['data' => $res]);
+		$resArray = $this->_formateRateRes($response);
+		//var_dump($resArray);exit;
+		
+		return $this->render('index',['data' => $resArray]);
 	}	
 
-	public function getRateByPHP(){
+	private function _getRateByPHP(){
 		$url = 'http://www.rong360.com/licai/netloan/ajaxnetloanrating';
 		$ch = curl_init();
 		curl_setopt($ch,CURLOPT_URL,$url);
@@ -47,7 +49,55 @@ class RateController extends BaseController
 			print curl_error($ch);
 		}
 		curl_close($ch);
-
 		return $response;
+	}
+
+	private function _formateRateRes($response){
+		//去掉字符串中(MISSING)、&nbsp;等字样
+		$res = str_replace('(MISSING)','',$response);
+		$res = str_replace(array("&nbsp;","&amp;nbsp;","\t","\r\n","\r","\n"),array("","","","","",""),$res);
+		//php内置函数去掉html、xml、php标签，并去除开始和结束的空白
+		$res = trim(strip_tags($res));
+		//将多个空白变成一个空白
+		$res = preg_replace('/\s(?=\s)/', '', $res); 
+		//最后，去掉非space 的空白，用一个空格代替 
+		$res = preg_replace('/[\n\r\t]/', ' ', $res); 
+		//将字符串拆分成数组，每9个为一组,舍弃最后一个‘了解详情’
+		$res = explode(' ', $res);
+		foreach ($res as $key => $value) {
+			$count = floor($key/8);
+			if ($key == 0) {
+				$resArray[0]['org_name'] = $value;
+				continue;
+			}
+			switch ($key%8) {
+				case 0:
+					$resArray[$count]['org_name'] = $value;
+					break;
+				case 1:
+					$resArray[$count]['rate'] = $value;
+					break;
+				case 2:
+					$resArray[$count]['average_income'] = $value;
+					break;
+				case 3:
+					$resArray[$count]['build_time'] = $value;
+					break;
+				case 4:
+					$resArray[$count]['backgroud'] = $value;
+					break;
+				case 5:
+					$resArray[$count]['hot'] = $value;
+					break;
+				case 6:
+					$resArray[$count]['high_praise_rate'] = $value;
+					break;
+				case 7:
+					break;
+				default:
+					break;
+			}
+		}
+		return $resArray;
 	}
 }
--- frontend/views/rate/index.php
+++ frontend/views/rate/index.php
@@ -4,6 +4,49 @@ $this->title = Yii::t('common','Rating');
 $this->params['breadcrumbs'][] = $this->title;
 
 ?>
-<table class="rate-table">
-<?= $data; ?>
-</table>
+<div class="row table-responsive">
+	<div class="col-lg-12">
+		<div style="border-bottom: 3px solid #e26854;">
+			<span style="color: #e26854; font-size: 28px;">网贷平台评级&nbsp;</span>
+			<b style="color: gray;"><?= Date('Y年m月d日') ?>更新</b>
+		</div>
+		<div style="border-bottom: 1px dashed gray; padding: 12px 5px;">
+			<p>1.本网贷评级结果仅供参考，不构成投资建议，本课题组不对投资者据此操作造成的损失承担后果；</p>
+			<p>2.评级参考的信息与数据不依赖网贷平台主动提供，且评级项目的所有经费由本站承担，凡以本课题组名义， 以任何理由向被评级对象或利益相关方收取费用的行为均属违规行为，如发现类似情况可及时向我们举报， 举报邮箱：735407073@qq.com</p>
+		</div>
+	</div>
+</div>
+<div class="row table-responsive">
+	<div class="col-lg-12">
+		<table class="table table-striped table-hover">
+			<thead>
+				<tr>
+					<th>综合排名</th>
+					<th>平台</th>
+					<th>评级</th>
+					<th>平均收益</th>
+					<th>上线时间</th>
+					<th>平台背景</th>
+					<th>人气指数</th>
+					<th>网友评价</th>
+					<th></th>
+				</tr>
+			</thead>
+			<tbody>
+				<?php foreach($data as $key => $value): ?>
+				<tr>
+					<td><?= $key+1 ?></td>
+					<td><a href="#"><?= $value['org_name'] ?></a></td>
+					<td style="font-weight: bold;"><?= $value['rate'] ?></td>
+					<td style="color: #bd0000;"><?= $value['average_income'] ?></td>
+					<td><?= $value['build_time'] ?></td>
+					<td><?= $value['backgroud'] ?></td>
+					<td style="color: red;"><?= $value['hot'] ?></td>
+					<td><?= $value['high_praise_rate'] ?></td>
+					<td><a href="#">了解更多</a></td>
+				</tr>
+				<?php endforeach;?> 
+			</tbody>
+		</table>
+	</div>
+</div>
