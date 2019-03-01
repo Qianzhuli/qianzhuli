0427381108fb2d7e75e839e3f6609777b6b2d986
201502lisihao <735407073@qq.com>
2019-03-01 16:28:09 +0800

    首页增加标签页插件

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -21,8 +21,35 @@ $this->title = Yii::t('common','Qianzhuli-a trusted loan rating website');
             <div class="col-lg-9">
                 <!-- 图片轮播组件 frontend/widgets/bannner -->
                 <?= BannerWidgets::widget() ?>
-                <div class="row">
-                    <div class="col-lg-4">
+            </div>
+            <div class="col-lg-3" style="text-align: center;">
+                <ul id="myTab" class="nav nav-tabs">
+                    <li class="active">
+                        <a href="#home" data-toggle="tab">
+                             内容1
+                        </a>
+                    </li>
+                    <li><a href="#ios" data-toggle="tab">iOS</a></li>
+                    <li><a href="#java" data-toggle="tab">java</a></li>
+                </ul>
+                <div id="myTabContent" class="tab-content">
+                    <div class="tab-pane fade in active" id="home">
+                        <p>菜鸟教程是一个提供最新的web技术站点，本站免费提供了建站相关的技术文档，帮助广大web技术爱好者快速入门并建立自己的网站。菜鸟先飞早入行——学的不仅是技术，更是梦想。</p>
+                    </div>
+                    <div class="tab-pane fade" id="ios">
+                        <p>iOS 是一个由苹果公司开发和发布的手机操作系统。最初是于 2007 年首次发布 iPhone、iPod Touch 和 Apple 
+                            TV。iOS 派生自 OS X，它们共享 Darwin 基础。OS X 操作系统是用在苹果电脑上，iOS 是苹果的移动版本。</p>
+                    </div>
+                    <div class="tab-pane fade" id="java">
+                        <p>jMeter 是一款开源的测试软件。它是 100% 纯 Java 应用程序，用于负载和性能测试。</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- 第二部分 -->
+        <div class="row">
+                    <div class="col-lg-3">
                         <h2>Heading</h2>
 
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
@@ -32,7 +59,7 @@ $this->title = Yii::t('common','Qianzhuli-a trusted loan rating website');
 
                         <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
                     </div>
-                    <div class="col-lg-4">
+                    <div class="col-lg-3">
                         <h2>Heading</h2>
 
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
@@ -42,7 +69,7 @@ $this->title = Yii::t('common','Qianzhuli-a trusted loan rating website');
 
                         <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
                     </div>
-                    <div class="col-lg-4">
+                    <div class="col-lg-3">
                         <h2>Heading</h2>
 
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
@@ -52,14 +79,18 @@ $this->title = Yii::t('common','Qianzhuli-a trusted loan rating website');
 
                         <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
                     </div>
-                </div>
-            </div>
-            <div class="col-lg-3" style="text-align: center;">
-                
-            </div>
-        </div>
+                    <div class="col-lg-3">
+                        <h2>Heading</h2>
 
-        <!-- 第二部分 -->
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                            ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                            fugiat nulla pariatur.</p>
+
+                        <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+                    </div>
+        </div>
+        <!-- 第三部分 -->
         <div class="row">
             <div class="col-lg-12">
                 <!-- 文章列表组件 frontend/widgets/posts -->
